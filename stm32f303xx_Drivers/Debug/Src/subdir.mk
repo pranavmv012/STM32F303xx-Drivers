cec7dfa74d6cddb249a14371b60823c09dea67a6
DESKTOP-O7Q82GF\Pranav <pranavmv012@gmail.com>
2023-01-19 17:32:11 -0500

    Revert "added gitignore, fixed compiling error of main "
    This reverts commit 038ef4919f40d53dbfe27a3b61d079b915e3e009.

--- stm32f303xx_Drivers/Debug/Src/subdir.mk
+++ stm32f303xx_Drivers/Debug/Src/subdir.mk
@@ -5,17 +5,17 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Src/main.c \
+../Src/003_ButtonIrqLed.c \
 ../Src/syscalls.c \
 ../Src/sysmem.c 
 
 OBJS += \
-./Src/main.o \
+./Src/003_ButtonIrqLed.o \
 ./Src/syscalls.o \
 ./Src/sysmem.o 
 
 C_DEPS += \
-./Src/main.d \
+./Src/003_ButtonIrqLed.d \
 ./Src/syscalls.d \
 ./Src/sysmem.d 
 
@@ -27,7 +27,7 @@ Src/%.o: ../Src/%.c Src/subdir.mk
 clean: clean-Src
 
 clean-Src:
-	-$(RM) ./Src/main.d ./Src/main.o ./Src/syscalls.d ./Src/syscalls.o ./Src/sysmem.d ./Src/sysmem.o
+	-$(RM) ./Src/003_ButtonIrqLed.d ./Src/003_ButtonIrqLed.o ./Src/syscalls.d ./Src/syscalls.o ./Src/sysmem.d ./Src/sysmem.o
 
 .PHONY: clean-Src
 
