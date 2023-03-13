74e75f569e0e400b2ec17498667f23a030d5c99f
DESKTOP-O7Q82GF\Pranav <pranavmv012@gmail.com>
2023-03-13 19:33:41 -0400

    i2c files added.
    added gitignore

---
+++ stm32f303xx_Drivers/Drivers/Inc/stm32f303xx_i2c_drv.h
@@ -0,0 +1,16 @@
+/*
+ *====================================================================================
+ *
+ * Name				:stm32f303xx_i2c_drv.h
+ * Description:		:Peripheral specific header file for i2c of stm32f303xx.
+ * Created on       :2023/03/12
+ * Author           :Pranav Vasudevan- pranavmv012@gmail.com
+ *=====================================================================================
+ */
+
+#ifndef INC_STM32F303XX_I2C_DRV_H_
+#define INC_STM32F303XX_I2C_DRV_H_
+
+
+
+#endif /* INC_STM32F303XX_I2C_DRV_H_ */
---
+++ stm32f303xx_Drivers/Drivers/Src/stm32f303xx_i2c_drv.c
@@ -0,0 +1,12 @@
+/*
+ *=====================================================================================
+ *
+ * Name				:stm32f303xx_i2c_drv.c
+ * Description		:This source file implements the I2C apis for the user application
+ * 					 for stm32f303xx.
+ * Created on       :2022/12/30
+ * Author           :Pranav Vasudevan- pranavmv012@gmail.com
+ *=====================================================================================
+ */
+
+
