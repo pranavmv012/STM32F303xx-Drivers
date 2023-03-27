c6b25775135172438c1034b42b1ae28d25f22730
DESKTOP-O7Q82GF\Pranav <pranavmv012@gmail.com>
2023-03-26 23:51:56 -0400

    I2C p clock control fn def

--- stm32f303xx_Drivers/Drivers/Src/stm32f303xx_i2c_drv.c
+++ stm32f303xx_Drivers/Drivers/Src/stm32f303xx_i2c_drv.c
@@ -22,3 +22,44 @@ RCC_Reg_Def_t *piRCC = RCC;
 #define I2C2_PCLK_DI() (piRCC->RCC_APB1ENR &= ~(1 << 22) )
 #define I2C3_PCLK_DI() (piRCC->RCC_APB1ENR &= ~(1 << 30) )
 
+/*
+ * ===  FUNCTION  ======================================================================
+ *   Name		:  I2C_PCLKControl
+ *   Description:  Function to enable or disable peripheral clock of I2C
+ *   Inputs		:  Pointer to base address of the I2C port, Enable or disable variable
+ * Output/return:  None.
+ * =====================================================================================
+ */
+void I2C_PCLKControl(I2C_Reg_Def_t *pI2Cx, uint8_t ENorDI)
+{
+	 if(ENorDI == ENABLE)
+	  {
+		 if(pI2Cx == I2C1)
+		 {
+			I2C1_PCLK_EN();
+		 }else if(pI2Cx == I2C2)
+		 {
+			 I2C2_PCLK_EN();
+
+		 }else if(pI2Cx == I2C3)
+		 {
+			 I2C3_PCLK_EN();
+		 }
+    }
+	 else
+	 {
+		 if(pI2Cx == I2C1)
+		 {
+			I2C1_PCLK_DI();
+		 }else if(pI2Cx == I2C2)
+		 {
+			 I2C2_PCLK_DI();
+
+		 }else if(pI2Cx == I2C3)
+		 {
+			 I2C3_PCLK_DI();
+		 }
+   }
+}
+
+
